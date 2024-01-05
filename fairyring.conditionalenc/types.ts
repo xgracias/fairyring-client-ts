10c5c547e133e895c9cfb82c4d4a434daf8bab20
bullishgopher <49041460+bullishgopher@users.noreply.github.com>
2024-01-04 17:21:25 -0800

    chore: export conditional enc nonce

--- fairyring.conditionalenc/types.ts
+++ fairyring.conditionalenc/types.ts
@@ -1,3 +1,4 @@
 import { EncryptedTx } from './types/fairyring/conditionalenc/encrypted_tx';
+import { ConditionalencNonce } from './types/fairyring/conditionalenc/Conditionalenc_nonce';
 
-export { EncryptedTx };
+export { EncryptedTx, ConditionalencNonce };
